d17ef5b4ea5415e9e1da153ae6257c987e84f5a6
pratikvernekar <pratik.vernekar@robosoftin.com>
2022-09-23 17:32:59 +0530

    complex added

--- Complex/6.js
+++ Complex/6.js
@@ -1,29 +1,26 @@
 let a1 = [5, 8, 9, 3, 5, 7, 1, 3, 4, 9, 3, 5, 1, 8, 4],
   a2 = [3, 5, 7, 2],
-  c = [];
+  b = [];
+c = [];
 
 let hash = new Map();
 
 for (let i = 0; i < a1.length; i++) {
   hash.set(a1[i], hash.get(a1[i]) === undefined ? 1 : hash.get(a1[i]) + 1);
 }
-console.log(hash);
-b = [];
 
 for (let i = 0; i < a2.length; i++) {
   if (hash.has(a2[i])) {
-    
     b.push(...Array(hash.get(a2[i])).fill(a2[i]));
   }
 }
 
 for (let i = 0; i < a1.length; i++) {
-  if (b.includes(a1[i])===false) {
+  if (b.includes(a1[i]) === false) {
     c.push(a1[i]);
   }
-  c.sort()
+  c.sort();
 }
-let res=b.concat(c)
-
+let res = b.concat(c);
 
 console.log(res);
